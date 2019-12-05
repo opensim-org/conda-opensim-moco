fe235f7c18160abf90c8a39b1b144c1e5783fedc
Christopher Dembia <cld72@cornell.edu>
2019-12-04 16:48:25 -0800

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,29 @@
+cd opensim-moco-source
+git submodule update --init
+cd ..
+mkdir moco_dependencies_build
+cd moco_dependencies_build
+cmake ../opensim-moco-source/dependencies \
+    -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DOPENSIM_PYTHON_WRAPPING=on \
+    -DOPENSIM_INSTALL_UNIX_FHS=on \
+    -DCMAKE_INSTALL_PREFIX=../moco_dependencies_install
+make -j${CPU_COUNT} ipopt
+make -j${CPU_COUNT}
+cd ..
+mkdir build
+cd build
+cmake ../opensim-moco-source \
+    -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DMOCO_PYTHON_BINDINGS=on \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DMOCO_INSTALL_UNIX_FHS=on \
+    -DMOCO_DEPENDENCIES_DIR=../moco_dependencies_install
+
+make -j${CPU_COUNT}
+make -j${CPU_COUNT} install
+
+# TODO: Is there anything we want to delete?
+# TODO: Test that the necessary files exist.
