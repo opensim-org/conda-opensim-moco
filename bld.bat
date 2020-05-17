2cd32e37ad75b7d96864e1491cb3079a38417e7d
Christopher Dembia <cld72@cornell.edu>
2020-05-17 10:04:30 -0700

    Use ninja for Windows.

--- bld.bat
+++ bld.bat
@@ -3,24 +3,28 @@ cd ..
 mkdir moco_dependencies_build
 cd moco_dependencies_build
 cmake ../opensim-moco-source/dependencies ^
-    -G "%CMAKE_GENERATOR%" ^
+    -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DOPENSIM_PYTHON_WRAPPING=on ^
     -DOPENSIM_INSTALL_UNIX_FHS=on ^
     -DCMAKE_INSTALL_PREFIX=../moco_dependencies_install
 cmake . -LAH
-cmake --build . --config Release -- /maxcpucount:%CPU_COUNT%
+ninja
 cd ..
 mkdir build
 cd build
 cmake ../opensim-moco-source ^
+    -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DMOCO_PYTHON_BINDINGS=on ^
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DMOCO_INSTALL_UNIX_FHS=on ^
     -DBUILD_TESTING=off ^
     -DMOCO_DEPENDENCIES_DIR=../moco_dependencies_install
 cmake . -LAH
+ninja install
 
-cmake --build . --target INSTALL --config Release -- /maxcpucount:%CPU_COUNT%
-
+Rem TODO https://github.com/conda-forge/casadi-feedstock/blob/master/recipe/bld.bat
+Rem TODO https://github.com/conda/conda-build/wiki/Windows-recipe-patterns
 Rem TODO use env vars like LIBRARY_BIN  https://github.com/conda/conda-recipes/blob/master/libtiff/bld.bat
 Rem https://github.com/conda-forge/armadillo-feedstock/blob/37f0a20559aaddbaede9b54cc4eaf725c6d8a7c7/recipe/bld.bat
